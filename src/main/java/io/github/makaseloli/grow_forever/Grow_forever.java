9e869d9196406492fdd13019e939d5414edec2d1
makaseloli <114403771+makaseloli@users.noreply.github.com>
2026-02-21 19:15:11 +0900

    Initialize project with Forge mod setup and mixins to enable infinite growth for bamboo, cactus, kelp, sugar cane, and other plants

---
+++ src/main/java/io/github/makaseloli/grow_forever/Grow_forever.java
@@ -0,0 +1,9 @@
+package io.github.makaseloli.grow_forever;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Grow_forever.MODID)
+public class Grow_forever {
+    public static final String MODID = "grow_forever";
+}
