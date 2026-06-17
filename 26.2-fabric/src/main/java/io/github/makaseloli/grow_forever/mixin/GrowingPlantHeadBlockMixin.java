61673837f67f5a184989fd28c58f82edb77015c5
makaseloli <114403771+makaseloli@users.noreply.github.com>
2026-06-17 11:03:14 +0900

    add 26.2 support

---
+++ 26.2-fabric/src/main/java/io/github/makaseloli/grow_forever/mixin/GrowingPlantHeadBlockMixin.java
@@ -0,0 +1,19 @@
+package io.github.makaseloli.grow_forever.mixin;
+
+import net.minecraft.world.level.block.GrowingPlantHeadBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GrowingPlantHeadBlock.class)
+public abstract class GrowingPlantHeadBlockMixin {
+    @ModifyConstant(method = "isRandomlyTicking", constant = @Constant(intValue = 25))
+    private int growForever$ignoreMaxAgeForTicking(int vanillaMaxAge) {
+        return vanillaMaxAge + 1;
+    }
+
+    @ModifyConstant(method = "randomTick", constant = @Constant(intValue = 25))
+    private int growForever$ignoreMaxAgeForGrowth(int vanillaMaxAge) {
+        return vanillaMaxAge + 1;
+    }
+}
---
+++ 26.2-neo/src/main/java/io/github/makaseloli/grow_forever/config/ServerConfig.java
@@ -0,0 +1,9 @@
+package io.github.makaseloli.grow_forever.config;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class ServerConfig {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    public static final ModConfigSpec SPEC = BUILDER.build();
+}
